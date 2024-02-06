b77a022f4c6035b98b1811375d1b9b3722edf853
Cralak <115760654+Cralak@users.noreply.github.com>
2024-02-06 14:05:47 +0100

    ShowBoard almost done

--- chessweeper/src/Main.java
+++ chessweeper/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
+
+    //Creates board and fills playable squares with V for "Valid square"
+    public static String[][] InitializeBoard(int x,int y){
+        String[][] board = new String[x][y];
+        for(int i=1;i<=x-1;i++){
+            for(int j=1;j<=y-1;j++){
+                board[i][j] = "V";
+            }
+        }
+        return board;
+    }
+
+    //Prints out current board in cli
+    public static String ShowBoard(String[][] board){
+        String toString = "";
+        for(int i=1;i;i++){
+            for(int j=1;j<=y-1;j++){
+                board[i][j] = "V";
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world");
     }
 }
