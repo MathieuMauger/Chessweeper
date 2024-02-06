33ac56b25346932270cb069d6557ff91b377e1e2
Cralak <115760654+Cralak@users.noreply.github.com>
2024-02-06 16:33:01 +0100

    Revert "Merge branch 'cralaktox'"
    This reverts commit da2155d4dbf55bf6bad8a21e90ee96067923f917, reversing
    changes made to 115eafdfc0fb9c9d326e2935e89f94e79583ec7f.

--- chessweeper/src/View/View.java
+++ chessweeper/src/View/View.java
@@ -5,32 +5,26 @@ public class View {
     //Prints out current board in cli
     public static void ShowBoard(String[][] board){
         StringBuilder toString = new StringBuilder();
+        for(int i=1;i<= board.length-2;i++){
+            for(int j=1;j<=board[i].length-2;j++){
+                if(board[j][i].isEmpty()){
+                    toString.append("  ");
+                } else if(board[j][i].equals("V")){
+                    toString.append("⬜ ");
+                } else if(board[j][i].equals("1")){
+                    toString.append("\uD83D\uDFE6 ");
+                } else if(board[j][i].equals("2")) {
+                    toString.append("\uD83D\uDFE5 ");
+                } else if(board[j][i].equals("3")){
+                    toString.append("\uD83D\uDFE9 ");
+                } else if(board[j][i].equals("4")){
+                    toString.append("\uD83D\uDFE8 ");
+                } else{
                     toString.append("⬛ ");
                 }
-                //if valid square add white square
-                else if(board[i][j].equals("V")){
-                    toString.append("⬜ ");
-                }
-                //if square is player 1 add blue square
-                else if(board[i][j].equals("1")){
-                    toString.append("\uD83D\uDFE6 ");
-                }
-                //if square is player 2 add red square
-                else if(board[i][j].equals("2")){
-                    toString.append("\uD83D\uDFE5 ");
-                }
-                } 
-                else if(board[j][i].equals("3")){
-                    toString.append("\uD83D\uDFE9 ");
-                } 
-                else if(board[j][i].equals("4")){
-                    toString.append("\uD83D\uDFE8 ");
-                }
-            toString.append("║");
+            }
             toString.append("\n");
         }
-        toString.append("  ╚════════════════════════════════════╝\n");
-        toString.append("    A  B  C   D  E  F   G  H  I   J  K\n");
         System.out.println(toString);
     }
 }
